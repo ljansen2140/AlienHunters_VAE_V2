9fa53227d1c96e0ae785e160595dde7311238f33
Logan <ljansen2140@gmail.com>
2021-03-04 17:20:20 -0500

    Adding Base Files + Architecture

---
+++ vae2.py
@@ -0,0 +1,43 @@
+
+
+
+
+
+
+# VAE - New Architecture
+
+
+
+### Load Dataset
+
+
+
+
+
+### Sampling Layer
+
+
+
+
+### Build Encoder Layers
+
+### Compile Encoder Layers
+
+
+
+### Build Decoder Layers
+
+### Compile Decoder Layers
+
+
+
+### Loss Function
+
+
+
+
+### Build VAE
+
+
+
+### Image Processing
