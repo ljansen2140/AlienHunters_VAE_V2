99580f22096be8fd12015975ccee74f206e3c859
theoutherguy <theoutherguy@gmail.com>
2021-04-03 19:44:03 -0400

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -71,3 +71,78 @@ def load_im(manifest, num_imgs, dim):
 		return_data = np.concatenate((return_data, im_np))
 
 	return return_data
+
+
+#Loads all images from a specified Manifest
+def load_manifest(manifest, dim):
+	#return_data = np.empty((0,) + dim + (3,))
+	#reshape_size = dim + (3,)
+	image_list = []
+	for obj in manifest:
+		if obj == "":
+			continue
+		print("Loading: " + obj)
+		im = Image.open(obj)
+		im_np = np.asarray(im)
+
+		#im_np = im_np.reshape(reshape_size)
+
+		im_np = im_np /255.
+
+		#NOTE: Concatenate is slow and BAD, do not use!!!
+		#return_data = np.concatenate((return_data, im_np))
+
+		image_list.append(im_np)
+
+	return_data = np.array(image_list)
+	return return_data
+
+def load_manifest_count(manifest, dim, count):
+	#return_data = np.empty((0,) + dim + (3,))
+	#reshape_size = dim + (3,)
+	image_list = []
+	for i in range(count):
+		obj = manifest[i]
+		if obj == "":
+			continue
+		#print("Loading: " + obj)
+		im = Image.open(obj)
+		im_np = np.asarray(im)
+
+		#im_np = im_np.reshape(reshape_size)
+
+		im_np = im_np /255.
+
+		#NOTE: Concatenate is slow and BAD, do not use!!!
+		#return_data = np.concatenate((return_data, im_np))
+
+		image_list.append(im_np)
+
+	return_data = np.array(image_list)
+	return return_data
+
+
+def load_manifest_rand(manifest, dim, count):
+	#return_data = np.empty((0,) + dim + (3,))
+	#reshape_size = dim + (3,)
+	manifest = manifest[:-1]
+	image_list = []
+	max_rand = len(manifest) - 1
+	for i in range(count):
+		choice = random.randint(0, max_rand)
+		obj = manifest[random.randint(0, max_rand)]
+		#print("Loading: " + obj)
+		im = Image.open(obj)
+		im_np = np.asarray(im)
+
+		#im_np = im_np.reshape(reshape_size)
+
+		im_np = im_np /255.
+
+		#NOTE: Concatenate is slow and BAD, do not use!!!
+		#return_data = np.concatenate((return_data, im_np))
+
+		image_list.append(im_np)
+
+	return_data = np.array(image_list)
+	return return_data
