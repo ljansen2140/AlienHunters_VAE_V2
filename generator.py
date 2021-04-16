3a71a187760f08e0f0338ac6c3229e4f9204712b
Logan <ljansen2140@gmail.com>
2021-04-16 04:47:22 -0400

    Update Generation Flow

--- generator.py
+++ generator.py
@@ -23,36 +23,46 @@ from pipeline import load_im, load_manifest, load_manifest_count, load_manifest_
 #MNIST
 from keras.datasets import mnist
 import numpy as np
-# (x_train, _), (x_test, _) = mnist.load_data()
-# x_train = x_train.astype('float32') / 255.
-# x_test = x_test.astype('float32') / 255.
 
 
+# Random Data Generator
+# Returns a random normal distribution of a specified size
+def genRandData(size):
+	g1 = tf.random.get_global_generator()
+	o = g1.normal(shape=[1,size])
+	return o
+
+
+
+# - Data Needed for Loading the VAE
 #CIFAR10 Filename List for importer
 CIFAR10_Filenames = ['data_batch_1','data_batch_2','data_batch_3','data_batch_4','data_batch_5']
-
-#Import data from CIFAR10 Dataset. Expected 5000 images total.
-# NOTE: This should work properly...
-# TODO: Double check the data returned is what is expected
-# load_data_sets(file_list, data_id)
-# Default data ID is 3 for Cats - See data_builder.py for details
-
-LATENT_DIM = 32
+LATENT_DIM = 512
 HIDDEN_LAYER_DIM = 2048
 
-IMAGE_DIMENSIONS = (32,32)
+IMAGE_DIMENSIONS = (512,512)
 
 input_shape = IMAGE_DIMENSIONS + (3,)
 
-pic_data = datab.load_data_sets(CIFAR10_Filenames)
+#CIFAR 10 Data
+#pic_data = datab.load_data_sets(CIFAR10_Filenames)
 
-g1=tf.random.get_global_generator()
-print(g1.normal(shape=[1,32]))
+
+# Load the model parts
 
 decoder = tf.keras.models.load_model('model/VAE_decoder.h5')
+encoder = tf.keras.models.load_model('model/VAE_encoder.h5')
 VAE = tf.keras.models.load_model('model/VAE.h5')
 
-results = decoder.predict(g1.normal(shape=[1,32]))
+
+##########################################
+
+# Generate new images here
+
+new_im = genRandData(512)
+results = decoder.predict(new_im)
+
 
 plt.imshow(results[0], cmap = plt.cm.binary)
-plt.show()
+#plt.show()
+plt.savefig("GenImage.png")
