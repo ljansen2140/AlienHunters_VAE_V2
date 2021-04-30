f2450caedbc9aa9f537de475b372eb6548b922c9
Logan <ljansen2140@gmail.com>
2021-04-29 23:38:51 -0400

    pipeline.py doc

--- pipeline.py
+++ pipeline.py
@@ -5,6 +5,12 @@
 # Manifest Specifications:
 #	A manifest is a file that includes a series of space ' ' seperated absolute filepaths
 #
+# Functions Implemented Here:
+#	load_im - Depreciated, do not use
+#	load_manifest - Loads all images from a manifest
+#	load_manifest_count - Loads a set number of images in a deterministic way from a manifest
+#	load_manifest_rand - Loads a set number of images randomly from a manifest
+#
 
 
 #Necessary Imports
@@ -58,6 +64,8 @@ def load_im(manifest, num_imgs, dim):
 
 
 #Loads all images from a specified Manifest
+#manifest - List of all possible filepaths
+#dim - Size of the images to load (VARIABLE NOT IN USE/DEPRECIATED)
 def load_manifest(manifest, dim):
 	#return_data = np.empty((0,) + dim + (3,))
 	#reshape_size = dim + (3,)
@@ -67,32 +75,11 @@ def load_manifest(manifest, dim):
 
 	# Load each image individually
 	for obj in manifest:
+		# Sanity Check
+		# FIXME: Manifest likely shouldn't include this in the first place?
 		if obj == "":
 			continue
-		print("Loading: " + obj)
-		im = Image.open(obj)
-		im_np = np.asarray(im)
-
-		#im_np = im_np.reshape(reshape_size)
-
-		im_np = im_np /255.
-
-		#NOTE: Concatenate is slow and BAD, do not use!!!
-		#return_data = np.concatenate((return_data, im_np))
-
-		image_list.append(im_np)
-
-	return_data = np.array(image_list)
-	return return_data
-
-def load_manifest_count(manifest, dim, count):
-	#return_data = np.empty((0,) + dim + (3,))
-	#reshape_size = dim + (3,)
-	image_list = []
-	for i in range(count):
-		obj = manifest[i]
-		if obj == "":
-			continue
+		#Debug Print
 		#print("Loading: " + obj)
 		im = Image.open(obj)
 		im_np = np.asarray(im)
@@ -109,10 +96,48 @@ def load_manifest_count(manifest, dim, count):
 	return_data = np.array(image_list)
 	return return_data
 
-
-def load_manifest_rand(manifest, dim, count):
+# Loads a specified number of images from a manifest in a deterministic order
+#manifest - List of all possible filepaths
+#dim - Size of the images to load (VARIABLE NOT IN USE/DEPRECIATED)
+#count - Number of images to return
+def load_manifest_count(manifest, dim, count):
 	#return_data = np.empty((0,) + dim + (3,))
 	#reshape_size = dim + (3,)
+
+	# Create an empty list for concatenate work around
+	image_list = []
+	
+	# Load each image individually
+	for i in range(count):
+		obj = manifest[i]
+		# Sanity Check
+		# FIXME: Manifest likely shouldn't include this in the first place?
+		if obj == "":
+			continue
+		#Debug Print
+		#print("Loading: " + obj)
+		im = Image.open(obj)
+		im_np = np.asarray(im)
+
+		#im_np = im_np.reshape(reshape_size)
+
+		im_np = im_np /255.
+
+		#NOTE: Concatenate is slow and BAD, do not use!!!
+		#return_data = np.concatenate((return_data, im_np))
+
+		image_list.append(im_np)
+
+	return_data = np.array(image_list)
+	return return_data
+
+# Loads random images from a manifest
+# The best function implemented here for training
+#manifest - List of all possible filepaths
+#dim - Size of the images to load (VARIABLE NOT IN USE/DEPRECIATED)
+#count - Number of images to return
+def load_manifest_rand(manifest, dim, count):
+	
 	manifest = manifest[:-1]
 	image_list = []
 	max_rand = len(manifest) - 1
